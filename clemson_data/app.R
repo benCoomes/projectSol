1b540045ece8acd5302d9241b4516e8405b308bc
Ben Coomes <bcoomes@g.clemson.edu>
2016-04-12 14:34:20 -0400

    moved data fetching and cleaning outside of server() function in app, only run once per session.

--- clemson_data/app.R
+++ clemson_data/app.R
@@ -52,54 +52,54 @@ ui <- fluidPage(
   leafletOutput("mymap")
 )
 
-server <- function(input, output) {
-  
-  username <- "3af116fb-a68b-41e3-959b-29d1a83399d9-bluemix"
-  password <- "70d0f1455b7dd050452faa5d3650f0907bb98cde87f61fcde0cf57dbca8e3d7e"
-  
-  rawDataFrame <-  {
-    #http <- "https://3af116fb-a68b-41e3-959b-29d1a83399d9-bluemix.cloudant.com/clemson_data/_all_docs?limit=1000&include_docs=true&descending=true"
-    http <- "https://3af116fb-a68b-41e3-959b-29d1a83399d9-bluemix.cloudant.com/clemson_data/_all_docs?include_docs=true"
-    httpResponse <- GET(http, authenticate(username, password))
-    cat(file = stderr(), "Done recieveing data from cloudant\n")
-    
-    documents <- fromJSON(toString(content(httpResponse, as = "text" , encoding = "UTF-8")))
-    data <- documents[['rows']]
-    df <- data[['doc']]
-    
-    #select columns to be used an rename them. (leave our columns xcoor and ycoor)
-    df <- data.frame("id" = df$`_id`, 
-                     "rev" = df$`_rev`, 
-                     "lat" = df$lat, 
-                     "lng" = df$lng,
-                     "flux" = df$flux,
-                     "URI" = df$URI,
-                     "time" = df$timestamp)
+# Pull data outside of server... see what happens
 
-    
-    # remove incomplete rows from the data frame
-    # ignore missing URIs for the moment
-    df <- df[complete.cases(df),]
-    
-    #convert flux values to numbers
-    df$flux <- as.numeric(df$flux)
-    
-    # remove rows with invalid coordinates from the dataframe, don't know why these lines have to be seperate
-    df <- df[(df$lat >= -90.0), ]
-    df <- df[(df$lat <= 90.00), ]
-    df <- df[(df$lng >= -180.0), ]
-    df <- df[(df$lng <= 180.0), ]
-    
-    # make a vector with unique coordinates contained in df
-    ucoors <- data.frame(lat=df$lat, lng=df$lng) %>% unique()
-    
-    #conver time from char vector to POSIXct type 
-    df$time <- strptime(df$time, format = "%Y-%m-%d %H:%M:%S", tz = "GMT")
-    df$time <- as.POSIXct(df$time)
-    
-    # remove any newly-incomplete rows
-    df <- df[complete.cases(df), ]
-  } 
+username <- "3af116fb-a68b-41e3-959b-29d1a83399d9-bluemix"
+password <- "70d0f1455b7dd050452faa5d3650f0907bb98cde87f61fcde0cf57dbca8e3d7e"
+#http <- "https://3af116fb-a68b-41e3-959b-29d1a83399d9-bluemix.cloudant.com/clemson_data/_all_docs?limit=1000&include_docs=true&descending=true"
+http <- "https://3af116fb-a68b-41e3-959b-29d1a83399d9-bluemix.cloudant.com/clemson_data/_all_docs?include_docs=true"
+httpResponse <- GET(http, authenticate(username, password))
+cat(file = stderr(), "Done recieveing data from cloudant\n")
+
+documents <- fromJSON(toString(content(httpResponse, as = "text" , encoding = "UTF-8")))
+data <- documents[['rows']]
+df <- data[['doc']]
+
+#select columns to be used an rename them. (leave our columns xcoor and ycoor)
+df <- data.frame("id" = df$`_id`, 
+                 "rev" = df$`_rev`, 
+                 "lat" = df$lat, 
+                 "lng" = df$lng,
+                 "flux" = df$flux,
+                 "URI" = df$URI,
+                 "time" = df$timestamp)
+
+
+# remove incomplete rows from the data frame
+# ignore missing URIs for the moment
+df <- df[complete.cases(df),]
+
+#convert flux values to numbers
+df$flux <- as.numeric(df$flux)
+
+  # remove rows with invalid coordinates from the dataframe, don't know why these lines have to be seperate
+  df <- df[(df$lat >= -90.0), ]
+  df <- df[(df$lat <= 90.00), ]
+  df <- df[(df$lng >= -180.0), ]
+  df <- df[(df$lng <= 180.0), ]
+  
+  # make a vector with unique coordinates contained in df
+  ucoors <- data.frame(lat=df$lat, lng=df$lng) %>% unique()
+  
+  #conver time from char vector to POSIXct type 
+  df$time <- strptime(df$time, format = "%Y-%m-%d %H:%M:%S", tz = "GMT")
+  df$time <- as.POSIXct(df$time)
+  
+  # remove any newly-incomplete rows
+  df <- df[complete.cases(df), ]
+
+
+server <- function(input, output) {
   
   v <- reactiveValues(active_data = df)
   
